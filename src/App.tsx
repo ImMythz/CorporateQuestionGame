12634da9004542f7b94742ad42716c0cefabbba7
Nate Worley <immythz@gmail.com>
2022-01-27 23:56:07 -0600

    added routing, pages, and Home page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route, 
+  Routes,
+} from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-green-500'>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App;
