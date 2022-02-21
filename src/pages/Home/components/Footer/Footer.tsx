0a6f12c09f46a2bf847d2c15bd8d89ed71daa318
Nate Worley <immythz@gmail.com>
2022-02-21 16:43:36 -0600

    fixed responsive footer text

--- src/pages/Home/components/Footer/Footer.tsx
+++ src/pages/Home/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer className='flex flex-col items-center justify-center h-10 bg-navy fixed bottom-0 w-full text-white'>
-            <p className='text-white text-sm'>2022 Leadership Summit | © 2022, Infinite Electronics International, Inc. All Rights Reserved.</p>
+            <p id='footerText' className='text-white text-sm'>2022 Leadership Summit | © 2022, Infinite Electronics International, Inc. All Rights Reserved.</p>
         </footer>
     );
 }
