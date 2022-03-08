f2ce1660ab587ebc3d10d7c8412b15babae79fa9
Nate Worley <immythz@gmail.com>
2022-03-07 20:30:30 -0600

    adding closing modal function

--- src/pages/Home/components/GameBoard/QuestionComponents/QuestionTiles/Question1/Question1.tsx
+++ src/pages/Home/components/GameBoard/QuestionComponents/QuestionTiles/Question1/Question1.tsx
@@ -15,19 +15,57 @@ export default function Question1() {
     const { answer, color } = useSelector((state: any) => state.question1);
     const { test } = useSelector((state: any) => state.modalState);
     const dispatch = useDispatch()
+    const closeModal = () => dispatch(setThing(false));
     // const ref = useRef();
     // const closeTooltip = () => ref.current.close();
     // const [open, setOpen] = useState(false);
-    const closeModal = () => dispatch(setThing(false));
 
     return (
         <article className={`flex flex-col items-center justify-center h-40 w-40 bg-white blue border-2 border-navy ${color}`}>
-            <Popup open={test} closeOnDocumentClick  onClose={closeModal} trigger={<div className='h-full w-full font-bold flex flex-col items-center justify-center tileButton'>
-                                {questions[0].question}
-                                <p className='answer1 w-fit mt-2'>{answer}</p>
-                            </div>} modal nested>
-                            {/* <button type='button'className='button' onClick={closeTooltip}>toggle</button> */}
-                <Card />
+            <Popup 
+                open={test} 
+                closeOnDocumentClick  
+                onClose={closeModal} 
+                trigger={
+                    <div className='h-full w-full font-bold flex flex-col items-center justify-center tileButton'>
+                        {questions[0].question}
+                        <p className='answer1 w-fit mt-2'>{answer}</p>
+                    </div>
+                } 
+                modal 
+                nested
+            >
+                {close => (
+                    <div className='modal'>
+                        <button className="close" onClick={close}>
+                        &times;
+                        </button>
+                        <Card/>
+                        <div className="actions">
+                                <Popup
+                                    trigger={<button className="button"> Trigger </button>}
+                                    position="top center"
+                                    nested
+                                >
+                                    <span>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+                                    magni omnis delectus nemo, maxime molestiae dolorem numquam
+                                    mollitia, voluptate ea, accusamus excepturi deleniti ratione
+                                    sapiente! Laudantium, aperiam doloribus. Odit, aut.
+                                    </span>
+                                </Popup>
+                                <button
+                                    className="button"
+                                    onClick={() => {
+                                    console.log('modal closed ');
+                                    close();
+                                    }}
+                                >
+                                    close modal
+                                </button>
+                        </div>
+                    </div>
+                )}
             </Popup>
         </article>
     );
